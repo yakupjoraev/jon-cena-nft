0885b69dc53c223fa4f1691773210991377f1878
Yakup <yakupjoraev@gmail.com>
2023-02-04 21:47:48 +0300

    added new sections

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -55,3 +55,23 @@ function fixedNav() {
 }
 window.addEventListener('scroll', fixedNav)
 
+
+// Аккордеон
+function accordion() {
+  const items = document.querySelectorAll('.accordion__item-trigger')
+  items.forEach(item => {
+    item.addEventListener('click', () => {
+      const parent = item.parentNode
+      if (parent.classList.contains('accordion__item-active')) {
+        parent.classList.remove('accordion__item-active')
+      } else {
+        document
+          .querySelectorAll('.accordion__item')
+          .forEach(child => child.classList.remove('accordion__item-active'))
+        parent.classList.add('accordion__item-active')
+      }
+    })
+  })
+}
+accordion() 
+

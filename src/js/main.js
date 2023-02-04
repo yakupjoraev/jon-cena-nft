ebfd0ea672cc0420af63763d5549fc8b68d9335a
Yakup <yakupjoraev@gmail.com>
2023-02-04 23:39:07 +0300

    end

--- src/js/main.js
+++ src/js/main.js
@@ -72,4 +72,19 @@ function accordion() {
     })
   })
 }
-accordion() 
+accordion()
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+};

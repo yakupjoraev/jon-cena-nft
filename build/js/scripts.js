7b5fdbfbd9ca678dd2cbc6693e7d014467987d25
Yakup <yakupjoraev@gmail.com>
2023-02-05 17:03:31 +0300

    ff

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -90,46 +90,96 @@ for (let anchor of anchors) {
   })
 };
 
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
+// function getTimeRemaining(endtime) {
+//   var t = Date.parse(endtime) - Date.parse(new Date());
+//   var seconds = Math.floor((t / 1000) % 60);
+//   var minutes = Math.floor((t / 1000 / 60) % 60);
+//   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+//   var days = Math.floor(t / (1000 * 60 * 60 * 24));
+//   return {
+//     'total': t,
+//     'days': days,
+//     'hours': hours,
+//     'minutes': minutes,
+//     'seconds': seconds
+//   };
+// }
+
+// function initializeClock(id, endtime) {
+//   var clock = document.getElementById(id);
+//   var daysSpan = clock.querySelector('.days');
+//   var hoursSpan = clock.querySelector('.hours');
+//   var minutesSpan = clock.querySelector('.minutes');
+//   var secondsSpan = clock.querySelector('.seconds');
+
+//   function updateClock() {
+//     var t = getTimeRemaining(endtime);
+
+//     daysSpan.innerHTML = t.days;
+//     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+//     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+//     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+//     if (t.total <= 0) {
+//       clearInterval(timeinterval);
+//     }
+//   }
+
+//   updateClock();
+//   var timeinterval = setInterval(updateClock, 1000);
+// }
+
+// var deadline = "January 01 2018 00:00:00 GMT+0300"; //for Ukraine
+// var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+// initializeClock('countdown', deadline);
+// initializeClock('countdown-2', deadline);
+
+
+var newDate,
+  intervalId;
+if (localStorage["timer"]) {
+  newDate = new Date(localStorage["timer"]);
+  timer(); // Инициализация текста в элементах.
+  intervalId = setInterval(timer, 1000);
 }
 
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+function onStart() {
+  newDate = Date.now() + 24 * 60 * 60 * 1000; // Таймер на 60 минут
+  timer(); // Инициализация текста в элементах.
+  if (!intervalId) intervalId = setInterval(timer, 1000);
+  localStorage["timer"] = new Date(newDate).toISOString();
+}
 
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+function timer() {
+  var now = Date.now(),
+    rest = newDate >= now ? Math.round((newDate - now) / 1000) * 1000 : 0,
+    s = "";
+  if (!rest) {
+    clearInterval(intervalId);
+    intervalId = null;
+    localStorage.removeItem("timer");
+  } else {
+    var rest2 = rest % 3600000;
+    let hh = (rest - rest2) / 3600000;
+    rest = rest2;
 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
+    rest2 = rest % 60000;
+    let mm = (rest - rest2) / 60000;
+    rest = rest2;
+
+    rest2 = rest % 1000;
+    let ss = (rest - rest2) / 1000;
+    let ms = rest2;
+
+    s = (hh < 10 ? "0" : "") + hh + ":" +
+      (mm < 10 ? "0" : "") + mm + ":" +
+      (ss < 10 ? "0" : "") + ss;
   }
+  document.querySelector("#time").textContent = s;
+  document.querySelector("#time-2").textContent = s;
+  // document.querySelector("#start").style.display = 'none';
 
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
 }
-
-var deadline = "January 01 2018 00:00:00 GMT+0300"; //for Ukraine
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
-initializeClock('countdown', deadline);
-initializeClock('countdown-2', deadline);
+timer();
+// localStorage.clear();
